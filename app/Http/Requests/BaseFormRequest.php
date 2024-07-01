63f25dcf7e519b269628fa5f880ffbc9650651ad
Omar <omarwaleed750@gmail.com>
2024-07-01 18:14:57 +0300

    (api) feat: Add BaseFormRequest with JSON response for validation errors.

---
+++ app/Http/Requests/BaseFormRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BaseFormRequest extends FormRequest
+{
+    /**
+     * Override failedValidation method to return a JSON response.
+     *
+     * @param Validator $validator
+     * @throw HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
